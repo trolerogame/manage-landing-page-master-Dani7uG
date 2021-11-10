c5499cdb1a60eecbffadf7d5160793e3f7aa882b
Daniel Campos <danielgcampos.04@gmail.com>
2021-11-10 16:43:38 -0300

    json de comentarios y manage hechos

---
+++ src/components/Notify.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { GetStarted } from '../styles/styleHeader'
+import { ContainNotify } from '../styles/styleNotify'
+const Notify = () => 
+    <ContainNotify>
+        <h1>Simplify how your team works today.</h1>
+        <GetStarted responsive white>Get Started</GetStarted>
+    </ContainNotify>
+    
+
+
+export default Notify
---
+++ src/styles/styleNotify.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import BgMovile from '../../images/bg-simplify-section-mobile.svg'
+import BgDesktop from '../../images/bg-simplify-section-desktop.svg'
+
+
+export const ContainNotify = styled.div`
+    background:hsl(12, 88%, 59%) url(${BgMovile}) no-repeat;
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    padding:70px 0 90px 0;
+    h1{
+        color:#fff;
+        font-size:40px;
+        margin:15px 20px 25px 20px;
+    }
+    @media (min-width: 768px) {
+        flex-direction:row;
+        justify-content:space-between;
+        padding:50px 140px 70px 140px;
+        margin-top:250px;
+        h1{
+            width: 500px;
+            text-align:start;
+        }
+        background-image: url(${BgDesktop});
+        background-position: 100px -60px;
+        background-size:100%;
+    }
+`
